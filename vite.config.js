cf6bc7864ef2dd2c7eb90aba50793178971601d1
xenobyte012 <xenobyte012@gmail.com>
2026-03-06 11:12:46 +0200

    updating the contact numbers on whatsapp

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/teamworktutors-website",
+  base: "/teamworktutors-website/",
 });
