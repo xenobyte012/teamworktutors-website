3ff4df98567fee174d40fa97c178f50df71767a2
xenobyte012 <xenobyte012@gmail.com>
2026-03-08 01:55:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter '>
-      <App />
-    </BrowserRouter>
+    <StrictMode>
+        <BrowserRouter >
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+
 
 )
