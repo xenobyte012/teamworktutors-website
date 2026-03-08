e196bf0719ede193ba80ba70a56c1ff6ab0cca15
xenobyte012 <xenobyte012@gmail.com>
2026-03-08 22:56:04 +0200

    Add robots.txt a

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename='teamworktutors-website'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
 
