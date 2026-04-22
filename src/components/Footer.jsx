7ced708b3f419ffbba1f211932a399919b46f386
xenobyte012 <xenobyte012@gmail.com>
2026-04-22 00:00:23 +0000

    fixing the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom'
-import whatsapp from '../images/whatsapp.png'
-import facebook from '../images/facebook.png'
-import instagram from '../images/instagram.png'
-import twitter from '../images/twitter.png'
+// import assets
+import whatsapp from "../images/whatsapp.png";
+import facebook from "../images/facebook.png";
+import instagram from "../images/instagram.png";
+import twitter from "../images/twitter.png";
 
 function Footer() {
-
   return (
-    <div className="  bg-slate-900 flex flex-col   py-10 text-white text-h4 ">
-      <div className="flex flex-row justify-center gap-10 ">
-        <div>
-          <img loading='lazy' src={whatsapp} alt="whatsapp icon" className=" w-10" />
-        </div>
-        <div>
-          <img loading='lazy' src={facebook} alt="facebook icon" className=" w-10" />
-        </div>
-        <div>
-          <img loading='lazy' src={instagram} alt="instagram icon" className=" w-11" />
-        </div>
-        <div>
-          <img loading='lazy' src={twitter} alt="twitter icon" className=" w-10" />
-        </div>
+    // main container
+    <footer className="bg-slate-900 flex flex-col py-10 text-white text-sm">
+      
+      {/* Social Media Section */}
+      <div className="flex justify-center gap-8">
+
+        {/* Each icon is now clickable (IMPORTANT for real projects) */}
+        <a
+          href="https://wa.me/your-number"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={whatsapp} alt="WhatsApp" className="w-10 hover:scale-110 transition" />
+        </a>
+
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <img src={facebook} alt="Facebook" className="w-10 hover:scale-110 transition" />
+        </a>
+
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={instagram} alt="Instagram" className="w-11 hover:scale-110 transition" />
+        </a>
+
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img src={twitter} alt="Twitter" className="w-10 hover:scale-110 transition" />
+        </a>
       </div>
-      <div className="flex flex-row justify-center gap-2 my-4">
-        <div className="hover:text-blue-600">
-          <Link to="/">Home</Link>
-        </div>
-        <div>|</div>
-        <div className="hover:text-blue-600">
-          <Link to="/about">About Us</Link>
-        </div>
-        <div>|</div>
-        <div className="hover:text-blue-600">
-          <Link to="/contact">Contact Us </Link>
-        </div>
+
+      {/* Navigation Links */}
+      <div className="flex justify-center gap-3 my-4 text-gray-300">
+        <Link className="hover:text-blue-500 transition" to="/">Home</Link>
+        <span>|</span>
+        <Link className="hover:text-blue-500 transition" to="/about">About</Link>
+        <span>|</span>
+        <Link className="hover:text-blue-500 transition" to="/contact">Contact</Link>
       </div>
-      <div className="flex flex-row justify-center ">
-        <h2>
-          © 2026 <span className="p-2">|</span> Xenobyte Web Studio
-          <span className="p-2">|</span> All Right Reserved
-        </h2>
+
+      {/* Copyright */}
+      <div className="text-center text-gray-400 text-sm">
+        <p>
+          © {new Date().getFullYear()} | Xenobyte Web Studio | All Rights Reserved
+        </p>
       </div>
-    </div>
+    </footer>
   );
 }
 
-export default Footer
+export default Footer;
